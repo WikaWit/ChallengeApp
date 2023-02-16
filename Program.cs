237538c1cfc95ced7fa6d219f62349ce90e9849e
WikaWit <125383589+WikaWit@users.noreply.github.com>
2023-02-16 12:58:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿var names= new List <string>{"Adam", "Jan"};
+var numbers= new List<int>{1,2,3};
+
+foreach(var n in names)
+    {
+        Console.WriteLine(n);
+            foreach(var x in numbers)
+                {
+                 Console.WriteLine(x);
+                }
+            
+    }
